f5f140f76627da20ce1012a9b7643e55ef4f22f3
dineshkmr1999 <dineshjnanain@gmail.com>
2024-10-16 02:10:43 +0530

    third commit

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -4,7 +4,7 @@ public class BuildScript
 {
     public static void PerformBuild()
     {
-        string buildPath = System.Environment.GetCommandLineArgs()[4];  // The path to the build output.
+        string buildPath = System.Environment.GetCommandLineArgs()[4];  // The path to the build output done.
         BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, buildPath, BuildTarget.StandaloneOSX, BuildOptions.None);
     }
 }
