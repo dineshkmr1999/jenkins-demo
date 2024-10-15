246bc2f39b745571a5a5ab6a6dcb5fa1fdd7e2c2
dineshkmr1999 <dineshjnanain@gmail.com>
2024-10-16 02:07:14 +0530

    second commit

---
+++ Assets/Editor/BuildScript.cs
@@ -0,0 +1,10 @@
+using UnityEditor;
+
+public class BuildScript
+{
+    public static void PerformBuild()
+    {
+        string buildPath = System.Environment.GetCommandLineArgs()[4];  // The path to the build output.
+        BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, buildPath, BuildTarget.StandaloneOSX, BuildOptions.None);
+    }
+}
