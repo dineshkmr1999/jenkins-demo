064d73e64064d66a06253f303576d8ed5f6cbbb4
dineshkmr1999 <dineshjnanain@gmail.com>
2024-10-16 19:04:14 +0530

    11th commit

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -29,6 +29,6 @@ public class BuildScript
         // Proceed with building the player
         BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, buildPath, BuildTarget.StandaloneWindows, BuildOptions.None);
 
-        Debug.Log("Build completed successfully!!!");
+        Debug.Log("Build completed successfully!!");
     }
 }
